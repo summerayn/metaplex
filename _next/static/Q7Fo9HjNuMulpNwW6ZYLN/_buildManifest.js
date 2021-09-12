711ab61bca5c4c9784d1821e9bc81639c742d43c
summerayn <hello@summerayn.com>
2021-09-12 15:59:08 +0700

    Updates

--- _next/static/Q7Fo9HjNuMulpNwW6ZYLN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-c3c462a95887b70be8f7.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
